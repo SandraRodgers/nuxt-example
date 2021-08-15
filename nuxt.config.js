33138e63dbe51bfbe04a746a16fb8e70c9991877
Sandra Rodgers <SRODG7@GMAIL.COM>
2021-08-15 10:34:58 -0500

    adds rss feed for each blog post

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,34 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    '@nuxtjs/feed',
+  ],
+
+  feed: [
+    {
+      path: '/feed.xml',
+      async create(feed) {
+        const { $content } = require('@nuxt/content');
+        const posts = await $content('blog').fetch();
+        feed.options = {
+          title: "Deepgram blog",
+          link: 'https://localhost:3000/feed.xml',
+          description: "All things Deepgram",
+          }
+
+          posts.forEach((post) => {
+            feed.addItem({
+              title: post.title,
+              id: post.slug,
+              author: post.author,
+              link: `http://localhost:3000/blog/${post.slug}`,
+              date: new Date(post.date),
+            })
+        })
+      },
+      cacheTime: 1000 * 60 * 15,
+      type: 'rss2',
+    }
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
